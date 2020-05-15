4c0801a35e2f676647c26b334538e8ffde2b3054
terminal <rgill0030@gmail.com>
2020-05-15 16:22:01 -0400

    Implemented the protobufs

--- ArtemisFaker/FakerModelInterfaceLayer.py
+++ ArtemisFaker/FakerModelInterfaceLayer.py
@@ -22,6 +22,8 @@ methods within a provider class.
 Returns a dict containing the method
 name, as well as the 'live' method.
 """
+
+
 class FakerRelicShimFactory():
 
     def __init__(self, ClassName, ProviderName, GeneratableNames):
@@ -30,7 +32,7 @@ class FakerRelicShimFactory():
         self.generatableNames = GeneratableNames
         self.liveClass = getattr(ipl.import_module(ProviderName), ClassName)
         self.get_available_ordered_dicts()
-    
+
     def get_available_ordered_dicts(self):
         """
         Instantiates access to all the entries in
@@ -40,13 +42,13 @@ class FakerRelicShimFactory():
         """
         for method in self.generatableNames:
             self.methods[method] = getattr(self.liveClass, method)
-    
+
     def return_available_generatables(self):
         """
         Returns back the self.methods object.
         """
         return self.methods
-    
+
     def help(self):
         """
         Returns out a list of the attributes
@@ -55,22 +57,34 @@ class FakerRelicShimFactory():
         """
         return vars(self.liveClass)
 
+
 """
 Generates an ordered dictonary provider from
 a fed in schema using the FakerRelicShimFactory
 to provide access to the ordered dicts within
 the old generator method.
 """
+
+
 class ODProviderFactory(FakerRelicShimFactory):
 
-    def __init__(self, ClassName, ProviderName, GeneratableNames, DatumSchema, UseFaker=True, GeneratorDicts=None):
+    def __init__(self, ConfigProto, UseFaker=True, GeneratorDicts=None):
+        message_path = ConfigProto.GenerationParameters
+        mod_mp = message_path.mod_conf
+        ClassName = mod_mp.classname
+        ProviderName = mod_mp.providername
+        GeneratableNames = []
+        for name in mod_mp.generatable:
+            GeneratableNames.append(name)
+        self.schema = []
+        for unit in message_path.schema:
+            self.schema.append(unit)
         if UseFaker is True:
             super().__init__(ClassName, ProviderName, GeneratableNames)
             self.generators = super().return_available_generatables()
             self.available_methods = {}
         elif UseFaker is False:
             self.available_methods = GeneratorDicts
-        self.schema = DatumSchema
 
     def produce_available_methods(self, GeneratorList):
         """
@@ -88,14 +102,15 @@ class ODProviderFactory(FakerRelicShimFactory):
         Create datum from format array and argument array
         Implements numpy RNG system. Functions for all
         ordered dict generation procedures.
-        """ 
+        """
         output = ""
         i = 0
-        end = len(self.schema) 
+        end = len(self.schema)
         for method in self.schema:
-            i -=- 1 
+            i -= - 1
             method_dict = self.available_methods[method]
-            injection = choice(method_dict["generatables"], 1, p=method_dict["weights"])
+            injection = choice(
+                method_dict["generatables"], 1, p=method_dict["weights"])
             if i != end:
                 output = output + injection[0] + Seperator
             else:
